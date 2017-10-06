3a53acf6214c2d8f1fc8e3d4e2b35dc528f8c6c1
ssoso27 <sohi8290@jejunu.ac.kr>
2017-10-07 00:25:01 +0900

    엔터 -> new Todo 저장

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -9,4 +9,15 @@ var todoStringField = getDom('todoString');
 todoStringField.addEventListener('keyup', function(event)
 {
   console.log('todoStringField keyup!');
+
+  // 엔터 입력 -> 새로운 todo값 저장
+  if (event.keyCode === 13)
+  {
+    console.log(todoStringField.value);
+
+    var newTodo = todoStringField.value;
+    todoStringField.value = "";
+
+    console.log('새로운 todo : ', newTodo);
+  }
 });
