ecfa5806286a0346eb1e096e19c314b39f6c3e28
ssoso27 <sohi8290@jejunu.ac.kr>
2017-10-07 00:01:33 +0900

    getDom

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -1,1 +1,11 @@
-//start...
+console.log('todos.js');
+
+// id에 해당하는 document를 return
+function getDom(id)
+{
+  return document.getElementById(id);
+}
+
+var todoStringField = getDom('todoString');
+
+console.log(todoStringField);
