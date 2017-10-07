8ba85068d6d84dbd01f458ebc772e2355c659bee
ssoso27 <sohi8290@jejunu.ac.kr>
2017-10-07 23:27:41 +0900

    공통함수 util.js로 빼기

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -1,34 +1,3 @@
-// id에 해당하는 document를 return
-function getDom(id)
-{
-  return document.getElementById(id);
-}
-
-// todo를 저장하는 함수
-function addTodo(event)
-{
- // 엔터 입력 -> 새로운 todo값 저장
- if (event.keyCode === 13)
- {
-   console.log(todoStringField.value);
-
-   var newTodo = todoStringField.value;
-   todoStringField.value = "";
-
-   console.log('새로운 todo : ', newTodo);
-
-   // innerHTML로 집어넣기
-   var listDom = getDom('todoList');
-
-   listDom.innerHTML += '\
-   <li>\
-     <button class="delete">x</button>\
-     <input type="checkbox" class="toggle-checked">\
-     <span class="text"> ' + newTodo + ' </span>\
-   </li>'
- }
-}
-
 // 할 일 입력 Field
 var todoStringField = getDom('todoString');
 todoStringField.addEventListener('keyup', addTodo);
