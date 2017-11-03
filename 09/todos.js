7461f236747ed59e2c42f3fb922ac67b76ac8fdb
ssoso27 <sohi8290@jejunu.ac.kr>
2017-11-03 17:08:50 +0900

    addli 함수 생성

--- 09/todos.js
+++ 09/todos.js
@@ -4,15 +4,24 @@ console.log("연결");
 var inputStringField = document.getElementById('inputString');
 var todoString ="";
 
+// div.container -> ul 안에 새로운 li 추가
+function addli()
+{
+  console.log('add li!');
+}
+
 inputStringField.addEventListener('keyup', function(event)
 {
   console.log("keyup");
 
-  // 엔터 -> tosoString에 저장 
+  // 엔터 -> tosoString에 저장
   if (event.keyCode === 13)
   {
     var inputKey = event.currentTarget.value;
     todoString = inputKey;
+
+    // todo : todoString을 div.container -> ul 안의 새 li를 만들어서 넣기
+    addli();
   }
 
   console.log(todoString);
