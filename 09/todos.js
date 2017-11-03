5bab0c0661c8ce0f28c2316d38e11d8a3d86e938
ssoso27 <sohi8290@jejunu.ac.kr>
2017-11-03 16:31:32 +0900

    inputString keyup 이벤트 연결

--- 09/todos.js
+++ 09/todos.js
@@ -1,2 +1,9 @@
 //start...
 console.log("연결");
+
+var inputStringField = document.getElementById('inputString');
+
+inputStringField.addEventListener('keyup', function(event)
+{
+  console.log("keyup");
+});
