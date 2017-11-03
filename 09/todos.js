7ee403ca04f1d2b4e5a9b65bf00e622b9806dbef
ssoso27 <sohi8290@jejunu.ac.kr>
2017-11-03 16:41:19 +0900

    엔터 -> tosoString에 저장

--- 09/todos.js
+++ 09/todos.js
@@ -2,8 +2,18 @@
 console.log("연결");
 
 var inputStringField = document.getElementById('inputString');
+var todoString ="";
 
 inputStringField.addEventListener('keyup', function(event)
 {
   console.log("keyup");
+
+  // 엔터 -> tosoString에 저장 
+  if (event.keyCode === 13)
+  {
+    var inputKey = event.currentTarget.value;
+    todoString = inputKey;
+  }
+
+  console.log(todoString);
 });
