eec0fd14ee45992033502dad35fc0b70ea09237e
ssoso27 <sohi8290@jejunu.ac.kr>
2017-12-01 17:49:09 +0900

    예제코드

--- 14/geolocation.js
+++ 14/geolocation.js
@@ -1,27 +1,29 @@
-// 지오로케이션
-function do_something(latitude, longitude)
-{
-  console.log("do_something");
-  console.log(latitude);
+console.log("geo 연결");
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>사용자의 브라우저는 지오로케이션을 지원하지 않습니다.</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>위도 : ' + latitude + '° <br>경도 : ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "사용자의 위치를 찾을 수 없습니다.";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
 }
-
-if ("geolocation" in navigator) {
-  /* 지오로케이션 사용 가능 */
-  console.log("사용가능");
-} else {
-  /* 지오로케이션 사용 불가능 */
-  console.log("사용불가");
-}
-
-// 사용자 현재 위치 확인 -> do_something() 실행
-navigator.geolocation.getCurrentPosition(function(position) {
-  do_something(position.coords.latitude, position.coords.longitude);
-});
-
-// 현재 위치 갱신
-var warchID = navigator.geolocation.watchPosition(function(position) {
-  do_something(position.coords.latitude, position.coords.longitude);
-})
-
-// 갱신 종료
-// navigator.geolocation.clearWatch(watchID);
